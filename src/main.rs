79f13c60d29193cd083a283e00376ec81513e9f5
Tom Ragle <tom@nonsensicalgames.com>
2018-10-20 15:28:13 -0400

    Use RNG

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 extern crate reqwest;
+extern crate rand;
+
+use rand::Rng;
+use rand::distributions::Alphanumeric;
 
 fn main() {
     println!("Starting fuzzman!");
     let client = reqwest::Client::new();
+    let str: String = rand::thread_rng()
+        .sample_iter(&Alphanumeric)
+        .take(100)
+        .collect();
     let _res = client.post("http://localhost:3000")
-        .form(&[("q", "hello")])
+        .form(&[("q", str)])
         .send();
 }
