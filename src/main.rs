d286b5e72d805aa6c7c6acb05824d313b2cc1261
Tom Ragle <tom@nonsensicalgames.com>
2018-10-20 15:33:52 -0400

    Send requests in a loop.

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,14 @@ use rand::distributions::Alphanumeric;
 fn main() {
     println!("Starting fuzzman!");
     let client = reqwest::Client::new();
-    let str: String = rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(100)
-        .collect();
-    let _res = client.post("http://localhost:3000")
-        .form(&[("q", str)])
-        .send();
+    let mut rng = rand::thread_rng();
+    for i in 1..20 {
+        let str: String = rng
+            .sample_iter(&Alphanumeric)
+            .take(100)
+            .collect();
+        let _res = client.post("http://localhost:3000")
+            .form(&[("q", str)])
+            .send();
+    }
 }
