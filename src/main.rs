d383137aad429e0743db0067921ce725394ed97f
Tom Ragle <tom@nonsensicalgames.com>
2018-10-20 14:58:54 -0400

    Simple POST request.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+extern crate reqwest;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting fuzzman!");
+    let client = reqwest::Client::new();
+    let _res = client.post("http://localhost:3000")
+        .form(&[("q", "hello")])
+        .send();
 }
