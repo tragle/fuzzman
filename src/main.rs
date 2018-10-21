5c2bdcee85cf37bb6619a8e208b5392cc277bb2f
Tom Ragle <tom@nonsensicalgames.com>
2018-10-20 20:04:57 -0400

    Add arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,46 @@
 extern crate reqwest;
 extern crate rand;
+extern crate num_cpus;
 
 use std::thread;
 use std::sync::mpsc;
+use std::env;
 use rand::Rng;
 use rand::distributions::Alphanumeric;
 
 fn main() {
     println!("Starting fuzzman!");
+
+    const WORD_SIZE: usize = 100;
+    let threads: usize = num_cpus::get() - 1;
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 1 {
+        return;
+    }
+    let default_arg = String::from("1000");
+    let requests_arg = &args.get(1).unwrap_or(&default_arg);
+    let requests = requests_arg.parse::<usize>().unwrap() / threads;
+
     let (tx, rx) = mpsc::channel();
     let mut count = 0;
+
     thread::spawn(move || {
         for _ in rx {
             count += 1;
             print!("\r{}", count);
         }
     });
-    for _ in 0..4 {
+
+    for _ in 0..threads {
         let child_tx = tx.clone();
         thread::spawn(move || {
             let client = reqwest::Client::new();
             let mut rng = rand::thread_rng();
-            for _i in 0..2500 {
+            for _i in 0..requests {
                 let str: String = rng
                     .sample_iter(&Alphanumeric)
-                    .take(10)
+                    .take(WORD_SIZE)
                     .collect();
                 let _ = client.post("http://localhost:3000")
                     .form(&[("q", str)])
